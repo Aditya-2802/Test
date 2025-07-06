78a9081ee615e6f1f6fc72ad61e3a0902359ff4d
Aditya Raj <124527941+Aditya-2802@users.noreply.github.com>
2025-07-07 01:42:38 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,1 @@
-from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://Aditya28:JqiJYcVXcrugeGO3@cluster0.dzs3uyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-)  # or use MongoDB Atlas URL
-db = client['Aditya']
-collection = db['git']
-
-def insert_event(data):
-    collection.insert_one(data)
-
-def get_all_events():
-    return list(collection.find().sort("timestamp", -1))
